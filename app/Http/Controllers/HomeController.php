5291c8aeb3198d9fafa0933028e8497d56f10ffb
muhammadhamdits <muhammadhamdi702@gmail.com>
2020-04-20 18:57:03 +0700

    Alhamdulillah

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,90 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Kriteria;
+use App\Alternatif;
+use App\AlternatifKriteria;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('home.index');
+        $kriteria = Kriteria::all();
+        $alternatif = Alternatif::all();
+        $best = "-";
+        $status = true;
+        $v = [];
+
+        foreach(Kriteria::all() as $k){
+            foreach($k->alternatifKriteria as $ak){
+                if($ak->kategori_id == null){
+                    $status = false;
+                }
+            }
+        }
+
+        if($status == true){
+            $x = [];
+            foreach(Kriteria::all() as $krit){
+                $xi = 0;
+                foreach(Alternatif::all() as $alt){
+                    $tmp = AlternatifKriteria::where('alternatif_id', $alt->id)
+                                        ->where('kriteria_id', $krit->id)
+                                        ->first()->kategori->nilai;
+                    $xi += $tmp*$tmp;
+                }
+                $x[] = sqrt($xi);
+            }
+
+            $r = [];
+            $ap = [];
+            $an = [];
+            foreach(Kriteria::all() as $key => $krit){
+                $ri = [];
+                $max = 0;
+                $min = 1000;
+                foreach(Alternatif::all() as $alt){
+                    $tmp = AlternatifKriteria::where('alternatif_id', $alt->id)
+                                        ->where('kriteria_id', $krit->id)
+                                        ->first()->kategori->nilai;
+                    $ri[] = ($tmp / $x[$key]) * $krit->bobot;
+                    if(($tmp / $x[$key]) * $krit->bobot > $max){
+                        $max = ($tmp / $x[$key]) * $krit->bobot;
+                    }
+                    if(($tmp / $x[$key]) * $krit->bobot < $min){
+                        $min = ($tmp / $x[$key]) * $krit->bobot;
+                    }
+                }
+                if($krit->jenis == 0){
+                    $tmp = $max;
+                    $max = $min;
+                    $min = $tmp;
+                }
+                $ap[] = $max;
+                $an[] = $min;
+                $r[] = $ri;
+            }
+            
+            $dp = [];
+            $dn = [];
+            $max = 0;
+            foreach(Alternatif::all() as $key => $alt){
+                $dpi = 0;
+                $dni = 0;
+                foreach(Kriteria::all() as $key2 => $krit){
+                    $tmp = $r[$key2][$key];
+                    $dpi += ($tmp - $ap[$key2]) * ($tmp - $ap[$key2]);
+                    $dni += ($tmp - $an[$key2]) * ($tmp - $an[$key2]);
+                }
+                $dp[] = sqrt($dpi);
+                $dn[] = sqrt($dni);
+                $v[] = sqrt($dni) / (sqrt($dni)+sqrt($dpi));
+                if(sqrt($dni) / (sqrt($dni)+sqrt($dpi)) > $max){
+                    $max = sqrt($dni) / (sqrt($dni)+sqrt($dpi));
+                    $best = $alt->nama;
+                }
+            }
+            
+        }
+        return view('home.index', compact('kriteria', 'alternatif', 'best', 'status', 'v'));
     }
 }
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -21,54 +21,40 @@
     <!-- Small boxes (Stat box) -->
     <div class="row">
 
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
             <!-- small box -->
             <div class="small-box bg-info">
                 <div class="inner">
-                    <h3>150</h3>
-                    <p>New Orders</p>
+                    <h3>{{ count($alternatif) }}</h3>
+                    <p>Alternatif</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-bag"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('alternatif.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
             <!-- small box -->
             <div class="small-box bg-success">
                 <div class="inner">
-                    <h3>53<sup style="font-size: 20px">%</sup></h3>
-                    <p>Bounce Rate</p>
+                    <h3>{{ count($kriteria) }}</h3>
+                    <p>Kriteria</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-stats-bars"></i>
                 </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-        
-        <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-                <div class="inner">
-                    <h3>44</h3>
-                    <p>User Registrations</p>
-                </div>
-                <div class="icon">
-                    <i class="ion ion-person-add"></i>
-                </div>
-                <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                <a href="{{ route('kriteria.index') }}" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
             </div>
         </div>
 
-        <div class="col-lg-3 col-6">
+        <div class="col-lg-4 col-6">
             <!-- small box -->
             <div class="small-box bg-danger">
                 <div class="inner">
-                    <h3>65</h3>
-                    <p>Unique Visitors</p>
+                    <h3>{{ $best }}</h3>
+                    <p>Alternatif Terbaik</p>
                 </div>
                 <div class="icon">
                     <i class="ion ion-pie-graph"></i>
@@ -86,18 +72,20 @@
                 <div class="card-header">
                     <h3 class="card-title">
                         <i class="fas fa-chart-pie mr-1"></i>
-                        Sales
+                        Grafik Hasil Perhitungan Topsis
                     </h3>
                 </div>
 
                 <div class="card-body">
                     <div class="tab-content p-0">
-                        <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
-                            <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>                         
+                        <div class="chart tab-pane active" id="chart" style="position: relative; height: 300px;">
+                            @if($status == true)
+                            <canvas id="chart-canvas" height="300" style="height: 300px;">
+                            </canvas>                         
+                            @else
+                            <div class="badge badge-danger"><h3>Data Belum Lengkap!</h3></div>
+                            @endif
                         </div>
-                        <div class="chart tab-pane" id="sales-chart" style="position: relative;     height: 300px;">
-                            <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>                         
-                        </div>  
                     </div>
                 </div>
             </div>
@@ -107,4 +95,36 @@
 @endsection
 
 @section('js')
+<script>
+    var l = [
+        @foreach($alternatif as $a)
+            "{{ $a->nama }}",
+        @endforeach
+    ];
+    var d = [
+        @foreach($v as $a)
+            {{ $a }},
+        @endforeach
+    ]
+    var barChartCanvas = $('#chart-canvas').get(0).getContext('2d');
+    var barChartOptions = {
+      responsive              : true,
+      maintainAspectRatio     : false,
+      datasetFill             : false
+    }
+
+    var barChart = new Chart(barChartCanvas, {
+        type: 'bar', 
+        data: {
+            labels: l,
+            datasets: [{
+                label: 'Prioritas',
+                data: d,
+                backgroundColor: "#dc3545",
+                borderWidth: 1
+            }]
+        },
+        options: barChartOptions
+    })
+</script>
 @endsection
